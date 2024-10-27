18272ba2dd80d48d3da762f13ea9f2e44d49a948
pandineer <pandineer@gmail.com>
2024-10-27 17:05:17 +0900

    chapter 2-4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 const chapter = process.argv[2];
 
@@ -55,6 +55,21 @@ switch (chapter) {
 
         // サブスクリプションの解除
         subscription.unsubscribe();
+        break;
+    case '2-4':
+        // 複数のオブザーバブルの定義
+        const observable41 = new Observable(subscriber => subscriber.next('Observable 41'));
+        const observable42 = new Observable(subscriber => subscriber.next('Observable 42'));
+
+        // サブスクリプションオブジェクトの作成
+        const allSubscriptions = new Subscription();
+
+        // オブザーバブルを購読し、サブスクリプションオブジェクトに追加
+        allSubscriptions.add(observable41.subscribe(value => console.log(value)));
+        allSubscriptions.add(observable42.subscribe(value => console.log(value)));
+
+        // 必要に応じてすべてのサブスクリプションを一括で解除
+        allSubscriptions.unsubscribe();
         break;
     default:
         console.log('無効なチャプターです');
