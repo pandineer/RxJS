e450443dff0aa967ef18d72c318e006e14d1acba
pandineer <pandineer@gmail.com>
2024-10-27 16:54:40 +0900

    chapter 2-2

--- src/index.js
+++ src/index.js
@@ -25,6 +25,24 @@ switch (chapter) {
             }
         });
         break;
+    case '2-2':
+        // オブザーバブルの作成
+        const observable = new Observable(subscriber => {
+            subscriber.next('Hello');
+            subscriber.next('World');
+            subscriber.complete();
+        });
+
+        // オブザーバの定義
+        const observer = {
+            next: value => console.log(`受け取った値: ${value}`),
+            error: error => console.error(`エラー発生: ${error}`),
+            complete: () => console.log('完了')
+        };
+
+        // オブザーバブルの購読
+        observable.subscribe(observer);
+        break;
     default:
         console.log('無効なチャプターです');
         break;
